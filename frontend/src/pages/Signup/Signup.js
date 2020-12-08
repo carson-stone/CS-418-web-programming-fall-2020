5575e1e635208cf4c0221ee1e5c81ce44e47b9f6
Carson Stone <carson.stone123@gmail.com>
2020-12-08 15:55:42 -0500

    added reCAPTCHA to signup page

--- frontend/src/pages/Signup/Signup.js
+++ frontend/src/pages/Signup/Signup.js
@@ -1,135 +1,153 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useMutation } from 'react-apollo';
 import gql from 'graphql-tag';
-import { validate } from 'graphql';
+import { ReCaptcha } from 'react-recaptcha-google';
 
 const SIGNUP_QUERY = gql`
-  mutation SignupQuery(
-    $email: String!
-    $password: String!
-    $phone: String!
-    $interest: String!
-  ) {
-    createUser(
-      email: $email
-      password: $password
-      phone: $phone
-      interest: $interest
-    ) {
-      user {
-        id
-      }
-    }
-  }
+	mutation SignupQuery(
+		$email: String!
+		$password: String!
+		$phone: String!
+		$interest: String!
+	) {
+		createUser(
+			email: $email
+			password: $password
+			phone: $phone
+			interest: $interest
+		) {
+			user {
+				id
+			}
+		}
+	}
 `;
 
 export default function () {
-  const history = useHistory();
+	const history = useHistory();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [phone, setPhone] = useState('');
-  const [interest, setInterest] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [phone, setPhone] = useState('');
+	const [interest, setInterest] = useState('');
 
-  const [valid, setValid] = useState('');
+	const [valid, setValid] = useState('');
 
-  const validate = () => {
-    if (
-      email.length === 0 ||
-      password.length < 5 ||
-      phone.length !== 10 ||
-      interest.length === 0
-    ) {
-      setValid(false);
-      return;
-    }
+	const recaptcha = useRef(null);
+	const [recaptchaCode, setRecaptchaCode] = useState(null);
 
-    setValid(true);
-  };
+	const verifyCallback = function (code) {
+		setRecaptchaCode(code);
+	};
 
-  useEffect(() => {
-    validate();
-  }, [email, password, phone, interest]);
+	const validate = () => {
+		if (
+			email.length === 0 ||
+			password.length < 5 ||
+			phone.length !== 10 ||
+			interest.length === 0
+		) {
+			setValid(false);
+			return;
+		}
 
-  const [signup, { loading, error }] = useMutation(SIGNUP_QUERY, {
-    onError: (error) => alert(error),
-    onCompleted: () => {
-      history.push('/login');
-    },
-  });
+		setValid(true);
+	};
 
-  if (loading) {
-    return <h2>loading</h2>;
-  }
+	useEffect(() => {
+		validate();
+	}, [email, password, phone, interest]);
 
-  return (
-    <div>
-      <h1>Singup</h1>
+	const [signup, { loading, error }] = useMutation(SIGNUP_QUERY, {
+		onError: (error) => alert(error),
+		onCompleted: () => {
+			history.push('/login');
+		},
+	});
 
-      {error && <h2>{error.message}</h2>}
+	if (loading) {
+		return <h2>loading</h2>;
+	}
 
-      <form>
-        <label htmlFor='email'>
-          Email
-          <input
-            name='email'
-            type='email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
+	return (
+		<div>
+			<h1>Singup</h1>
 
-        <label htmlFor='password'>
-          Password
-          <input
-            name='password'
-            type='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
+			{error && <h2>{error.message}</h2>}
 
-        <label htmlFor='phone'>
-          Phone
-          <input
-            name='phone'
-            type='tel'
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-          />
-        </label>
+			<form>
+				<label htmlFor='email'>
+					Email
+					<input
+						name='email'
+						type='email'
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</label>
 
-        <label htmlFor='interest'>
-          Interest
-          <input
-            name='interest'
-            type='text'
-            value={interest}
-            onChange={(e) => setInterest(e.target.value)}
-          />
-        </label>
-      </form>
+				<label htmlFor='password'>
+					Password
+					<input
+						name='password'
+						type='password'
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</label>
 
-      <div className='button-row-container'>
-        <span className='button-row'>
-          <button
-            className='primary'
-            onClick={(e) => {
-              e.preventDefault();
-              signup({
-                variables: { email, password, phone, interest },
-              });
-            }}
-            disabled={!valid}
-          >
-            Signup
-          </button>
-          <button className='secondary' onClick={() => history.push('/login')}>
-            Login
-          </button>
-        </span>
-      </div>
-    </div>
-  );
+				<label htmlFor='phone'>
+					Phone
+					<input
+						name='phone'
+						type='tel'
+						value={phone}
+						onChange={(e) => setPhone(e.target.value)}
+					/>
+				</label>
+
+				<label htmlFor='interest'>
+					Interest
+					<input
+						name='interest'
+						type='text'
+						value={interest}
+						onChange={(e) => setInterest(e.target.value)}
+					/>
+				</label>
+			</form>
+
+			<div className='button-row-container'>
+				<span className='button-row'>
+					<button
+						className='primary'
+						onClick={(e) => {
+							e.preventDefault();
+							if (recaptchaCode) {
+								signup({
+									variables: { email, password, phone, interest },
+								});
+							} else {
+								alert('You must verify with the ReCAPTCHA tool');
+							}
+						}}
+						disabled={!valid}
+					>
+						Signup
+					</button>
+					<button className='secondary' onClick={() => history.push('/login')}>
+						Login
+					</button>
+				</span>
+			</div>
+
+			<ReCaptcha
+				ref={recaptcha}
+				sitekey='6LcwGeQZAAAAAP6AYnyarMZE239hGgODGNQpFHom'
+				badge='inline'
+				verifyCallback={verifyCallback}
+			/>
+		</div>
+	);
 }
